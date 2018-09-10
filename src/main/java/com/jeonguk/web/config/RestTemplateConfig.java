73b2945e85b9d89eec490a4d934d90a2307c3bdc
jeonguk <kato1883@gmail.com>
2018-09-10 22:46:21 +0900

    HttpClientConfig & RestTemplateConfig 추가

--- src/main/java/com/jeonguk/web/config/RestTemplateConfig.java
+++ src/main/java/com/jeonguk/web/config/RestTemplateConfig.java
@@ -2,7 +2,6 @@ package com.jeonguk.web.config;
 
 import lombok.AllArgsConstructor;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
